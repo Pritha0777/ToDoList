e541f5d112cd6c27dd4f0eebe4d5c30ff4b14537
Pritha0777 <prithadey.official0777@gmail.com>
2024-08-02 11:21:09 +0530

    Added the list of options to perform in a todo list

--- ToDo.py
+++ ToDo.py
@@ -1,1 +1,10 @@
 #Code to create a simple To-Do List 
+
+#Main function
+while True:
+    print("\nOptions")
+    print("Display the list")
+    print("Add a new task")
+    print("Mark a task as completed")
+    print("Remove a task")
+    print("Exit")
