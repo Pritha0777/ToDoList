0a318ba9011088e99ba714cf348183373d113ada
Pritha0777 <prithadey.official0777@gmail.com>
2024-08-02 23:34:24 +0530

    Added the choice of action in an if-elif-else loop

--- ToDo.py
+++ ToDo.py
@@ -8,3 +8,28 @@ while True:
     print("Mark a task as completed")
     print("Remove a task")
     print("Exit")
+    action = input("Enter the choice of action: ")
+    if action == "1":
+       # display()
+       pass
+    elif action == "2":
+        task = input("Enter the task: ")
+       # add_task(task)
+    elif action == "3":
+        try:
+            #display()
+            task_num = int(input("Enter task number that needs to be marked as completed"))
+            #completed(task_num)
+        except ValueError:
+            print("Invalid input, enter a NUMBER")
+    elif action == "4":
+        try:
+            #display()
+            task_num = int(input("Enter the task number that needs to be removed"))
+           # removed(task_num)
+        except ValueError:
+            print("Invalid input, enter a NUMBER")
+    elif action == "5":
+        break
+    else:
+        print("Enter a valicd choice of action")
